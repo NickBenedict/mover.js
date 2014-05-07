3a2fba7bc4c4c5ea24ee1db8f1875e0bc2c20abc
Nick Benedict <nickben@outlook.com>
2014-05-06 23:02:29 -0400

    First mover.js commit

---
+++ mover.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(
+ 
+    function () {
+
+        enquire.register("screen and (max-width:479px)", {
+            match: function () {
+                $(".to-move").each(function () {
+                    var a = $(this);
+                    var b = a.data("move-0");
+                    $(a).detach();
+                    $("." + b).append($(a));
+                });
+            }
+        }
+)
+
+
+        enquire.register("screen and (min-width:480px) and  (max-width:786px) ", {
+            match: function () {
+                $(".to-move").each(function () {
+                    var a = $(this);
+                    var b= a.data("move-480");
+                    $(a).detach();
+                    $("."+b).append($(a));
+                });
+            }
+        }
+)
+
+
+        enquire.register("screen and (min-width:786px) and  (max-width:1023px)", {
+            match: function () {
+
+                $(".to-move").each(function () {
+                    var t = $(this);
+                    var l = t.data("move-786");
+                    $(t).detach();
+                    $("."+l).append($(t));
+                });                
+            }
+        }
+   )
+        enquire.register("screen and (min-width:1024px)", {
+            match: function () {
+
+                $(".to-move").each(function () {
+                    var t = $(this);
+                    var l = t.data("move-1024");
+                    $(t).detach();
+                    $("." + l).append($(t));
+                });
+            }
+        }
+)
+ });
